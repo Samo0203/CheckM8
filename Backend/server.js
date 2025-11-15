0b4b45ba56f595c47f3aad9230a3525d6b4f2240
Samo0203 <samodya2003samadini@gmail.com>
2025-11-15 22:58:20 +0530

    create backend

--- Backend/server.js
+++ Backend/server.js
@@ -18,4 +18,4 @@ app.use("/api", authRoutes);
 app.use("/api", otpRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
