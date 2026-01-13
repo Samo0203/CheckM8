a19f194eb1aec7335a226f27147d1470ba35e52b
Navodya Wijekoon <nyashikalaxp@gmail.com>
2026-01-13 17:09:29 +0530

    Change arrow models

--- Backend/models/Arrow.js
+++ Backend/models/Arrow.js
@@ -1,19 +1,6 @@
 import mongoose from "mongoose";
 
 const arrowSchema = new mongoose.Schema({
-<<<<<<< Updated upstream
-  from: { type: String, required: true },         // e.g., "e2"
-  to: { type: String, required: true },           // e.g., "e4"
-  color: { type: String, required: true },        // e.g., "green", "yellow"
-  number: { type: Number, required: true },       // the sequence number
-  user: { type: String, required: true },         // the logged-in user
-  variationID: { type: Number, default: 0 },     // variation ID, default to 0
-  createdAt: { type: Date, default: Date.now }   // timestamp for sorting
-});
-
-const Arrow = mongoose.model("arrow", arrowSchema);
-export default Arrow;
-=======
   from: { type: String, required: true },         
   to: { type: String, required: true },           
   color: { type: String, required: true },        
@@ -26,4 +13,3 @@ export default Arrow;
 });
 
 export default mongoose.model("Arrow", arrowSchema);
->>>>>>> Stashed changes
