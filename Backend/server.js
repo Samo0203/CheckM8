9d7c8e5012a9d885da18e2a30368938d6636be97
lakpreeth5902 <162424125+lakpreeth5902@users.noreply.github.com>
2026-01-13 20:35:48 +0530

    multiple chess boards

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import otpRoutes from "./routes/otp.js";
 import arrowRoutes from "./routes/arrows.js";
+import boardRoutes from "./routes/boards.js";
 
 dotenv.config();
 const app = express();
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api", authRoutes);
 app.use("/api", otpRoutes);
 app.use("/api", arrowRoutes);
+app.use("/api", boardRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
